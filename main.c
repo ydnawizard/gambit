efc0ef958f91ae1fe21a21c2080a4cd02bd7c54b
Andy <ydnawizard@protonmail.com>
2025-05-06 00:20:29 -0500

    Added string reading + realloc fail check in read_txt

--- main.c
+++ main.c
@@ -4,10 +4,17 @@
 int main()
 {
 	txt deck;
-	read_text_file_contents(&deck,"/home/andy/proj/c/gambit/decks/CCNA/CCNASect1");
+	read_text_file_contents(&deck,"/home/andy/proj/c/gambit/decks/Music/MajorScales/MajorScales");
 	for(int i=0;i<deck.character_count;i++)
 	{
 		printf("%c",deck.characters[i]);
 	}
+	printf("\n");
+	for(int i=0;i<deck.string_count;i++)
+	{
+		printf("%s\n",deck.strings[i]);
+		free(deck.strings[i]);
+	}
 	free(deck.characters);
+	free(deck.strings);
 }
--- src/read/read_txt.c
+++ src/read/read_txt.c
@@ -10,8 +10,8 @@ typedef struct text_file_contents{
 }txt;
 
 //Single-character handling
-void read_character_to_text_file_contents(
-		txt* text_file_contents,
+void read_character_to_text_file_contents_pointer(
+		txt* text_file_contents_pointer,
 		char character,
 		int character_index
 		)
@@ -20,49 +20,153 @@ void read_character_to_text_file_contents(
 	{
 		if(character != '\n' && character != ' ')
 		{
-			text_file_contents->characters
+			text_file_contents_pointer->characters
 				=malloc(sizeof(char));
-			text_file_contents->characters
-				[character_index]=character;
-			text_file_contents->character_count=1;
+			text_file_contents_pointer->characters
+				[character_index]
+				=character;
+			text_file_contents_pointer->character_count
+				=1;
 		}
 	}
 	else
 	{
 		if(character != '\n' && character != ' ')
 		{
-			text_file_contents->characters
-				=realloc(text_file_contents->characters,
+			void* tmp=realloc(text_file_contents_pointer->characters,
 						(character_index+1)*sizeof(char));
-			text_file_contents->characters
-				[character_index]=character;
-			text_file_contents->character_count+=1;
+			if(tmp != NULL)
+			{
+				text_file_contents_pointer->characters=tmp;
+				text_file_contents_pointer->characters
+					[character_index]
+					=character;
+				text_file_contents_pointer->character_count+=1;
+			}
 		}
 		else
 		{
-			text_file_contents->characters
-				=realloc(text_file_contents->characters,
+			void* tmp=realloc(text_file_contents_pointer->characters,
 						(character_index+1)*sizeof(char));
-			text_file_contents->characters
-				[character_index]='\0';
-			text_file_contents->character_count+=1;
+			if(tmp != NULL)
+			{
+				text_file_contents_pointer->characters=tmp;
+				text_file_contents_pointer->characters
+					[character_index]
+					='\0';
+				text_file_contents_pointer->character_count+=1;
+			}
 		}
 
 	}
 }
 
+//String handling
+void read_strings_to_text_file_contents_pointer(
+		txt* text_file_contents_pointer,
+		int *string_index,
+		int *string_character_index,
+		char character
+		)
+{
+	//first char first str
+	if((*string_character_index) == 0 && (*string_index) == 0)
+	{
+		if(character != ' ' && character != '\n')
+		{
+			text_file_contents_pointer->strings
+				=malloc(sizeof(char*));
+			text_file_contents_pointer->strings
+				[0]
+				=malloc(sizeof(char));
+			text_file_contents_pointer->strings
+				[0][0]
+				=character;
+			text_file_contents_pointer->string_count=0;
+			(*string_character_index)+=1;
+		}
+	}
+	//first char n str
+	else if((*string_character_index) == 0 && (*string_index) != 0)
+	{
+		if(character != ' ' && character != '\n')
+		{
+			void* tmp=realloc(text_file_contents_pointer->strings,
+						((*string_index)+1)*sizeof(char*));
+			if(tmp != NULL)
+			{
+				text_file_contents_pointer->strings=tmp;
+				text_file_contents_pointer->strings
+					[(*string_index)]
+					=malloc(sizeof(char));
+				text_file_contents_pointer->strings
+					[(*string_index)][(*string_character_index)]
+					=character;
+				(*string_character_index)+=1;
+			}
+		}
+	}
+	else
+	{
+		if(character != ' ' && character != '\n')
+		{
+			void* tmp=realloc(text_file_contents_pointer->strings
+						[(*string_index)],
+						((*string_character_index)+1)*sizeof(char));
+			if(tmp != NULL)
+			{
+				text_file_contents_pointer->strings
+					[(*string_index)]
+					=tmp;
+				text_file_contents_pointer->strings
+					[(*string_index)][(*string_character_index)]
+					=character;
+				(*string_character_index)+=1;
+			}
+		}
+		else
+		{
+			void* tmp=realloc(text_file_contents_pointer->strings
+						[(*string_index)],
+						((*string_character_index)+1)*sizeof(char));
+			if(tmp != NULL)
+			{
+				text_file_contents_pointer->strings
+					[(*string_index)]
+					=tmp;
+				text_file_contents_pointer->strings
+					[(*string_index)][(*string_character_index)]
+					='\0';
+				text_file_contents_pointer->string_count+=1;
+				(*string_index)+=1;
+				(*string_character_index)=0;
+			}
+		}
+	}
+}
+
+
+
 //main function
-void read_text_file_contents(txt* file_contents_pointer,char* file_path)
+void read_text_file_contents(txt* text_file_contents_pointer,char* file_path)
 {
 	char character;
-	int character_index=0;
+	int character_index=0,
+	    string_index=0,
+	    string_character_index=0;
 	FILE* file_pointer = fopen(file_path,"read");
 	while((character = fgetc(file_pointer))!=EOF)
 	{
-		read_character_to_text_file_contents(
-				&(*file_contents_pointer),
+		read_character_to_text_file_contents_pointer(
+				&(*text_file_contents_pointer),
 				character,
 				character_index
+				);
+		read_strings_to_text_file_contents_pointer(
+				&(*text_file_contents_pointer),
+				&string_index,
+				&string_character_index,
+				character
 				);
 		character_index+=1;
 	}
